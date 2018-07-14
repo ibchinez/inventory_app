898fa93af9a2e1819bd67051248540d99bef468d
Chrisgeek <chrisgeek29@gmail.com>
2018-07-14 08:56:11 +0100

    messages_contr

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,18 +1,19 @@
-  class MessagesController < ApplicationController
+class MessagesController < ApplicationController
 
-  def create
-   @message = Message.create(message_params) 	
-   if @message.save
-     respond_to do |format|
-      format.js{}
-    else 
-     respond_to do |format|
-      format.js{}
-    end
-  end
+ def create
+	@message = Message.create(message_params) 	
+	@message.save
+	respond_to do |format|
+          format.js{}
+       end
+  	else 
+  		respond_to do |format|
+          format.js{}
+        end
+ end
 
-  private
-  def message_params
-    params.require(:message).permit(:name, :email, :mobile_no, :message)
-  end
-  end
+ private
+	def message_params
+	   params.require(:message).permit(:name, :email, :mobile_no, :message)
+	end
+end
